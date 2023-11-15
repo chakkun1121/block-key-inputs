e23b43b5b316c6e885f9ec15a1306d20ac8a51e1
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-11-15 20:22:04 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const SUB_DIRECTORY = "/block-key-inputs";
+const isProd = process.env.NODE_ENV == "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"export"
+  output: "export",
+  basePath: isProd ? SUB_DIRECTORY : "",
+  assetPrefix: isProd ? SUB_DIRECTORY : "",
+  publicRuntimeConfig: {
+    basePath: isProd ? SUB_DIRECTORY : "",
+  },
 };
 const WithPWA = require("next-pwa");
 module.exports = WithPWA(nextConfig);
