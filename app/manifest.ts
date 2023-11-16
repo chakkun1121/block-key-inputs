e4a2d9e42c5bee130383fdedcde97d966558bf23
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-11-16 23:38:26 +0000

    アイコンの設定

---
+++ app/manifest.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "block-key-inputs",
+    description: "キー入力をブロックするアプリです。",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#f5a623",
+    theme_color: "#d0021b",
+    icons: [
+      {
+        src: "icon.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
